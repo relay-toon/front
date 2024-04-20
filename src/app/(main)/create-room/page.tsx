743f1e6d01e600b22e6e04eaf39b6f2273f71897
unknown <dhdytjq2@gmail.com>
2024-04-20 21:01:32 +0900

    feature: activate & deactivate LargeBtn on create-room

--- src/app/(main)/create-room/page.tsx
+++ src/app/(main)/create-room/page.tsx
@@ -2,13 +2,22 @@
 import LargeBtn from '@/src/components/LargeBtn';
 import { useEffect, useRef, useState } from 'react';
 import { moseoverHandler, timeSetMoseoverHandler } from './action';
+import { useForm, SubmitHandler } from 'react-hook-form';
+
+interface subjectValue {
+  drawingSubject: string;
+}
 
 export default function CreateRoom() {
+  const { register, handleSubmit } = useForm<subjectValue>();
   const [subjectText, setSubjectText] = useState('');
+  const [memberOption, setMemberOption] = useState(false);
+  const [timeOption, setTimeOption] = useState(false);
+
   const maxLength = 25;
   let hashtagArr = ['#어디에서', '#누가', '#무엇을', '#어떻게', '#하는것'];
-  let drawingTime = ['제한없음', '12초', '16초', '20초'];
   let memberNumArr = new Array(6).fill(0);
+  let drawingTime = ['제한없음', '12초', '16초', '20초'];
   const sliderRef = useRef<HTMLDivElement>(null);
   const timeSetSliderRef = useRef<HTMLDivElement>(null);
 
@@ -51,9 +60,16 @@ export default function CreateRoom() {
       }
     };
   }, []);
+  const handleSubjectTextChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSubjectText(e.target.value);
+  };
+  const onSubmit: SubmitHandler<subjectValue> = ({ drawingSubject }) => {};
+
+  const handleMemberCheck = () => [setMemberOption(true)];
+  const handleTimeCheck = () => [setTimeOption(true)];
 
   return (
-    <div className="max-w-sm mt-[23.5px] p-5 bg-white custom-waguri-font">
+    <div className="w-[390px] mt-[23.5px] p-5 bg-white custom-waguri-font h-[844px]">
       <div className="flex flex-col">
         <div className="flex justify-between">
           <span>드로잉 주제</span>
@@ -61,11 +77,16 @@ export default function CreateRoom() {
             {subjectText.length}/{maxLength}
           </span>
         </div>
-        <input
-          className="p-3 rounded-lg border border-[#9B9B9B] placeholder:text-[#9B9B9B] mt-[16px]"
-          type="text"
-          placeholder="멤버들과 그릴 그림 주제를 입력해주세요!"
-        />
+        <form onSubmit={handleSubmit(onSubmit)} className="w-screen mt-[16px]">
+          <input
+            {...register('drawingSubject', { required: true })}
+            className="w-[350px] p-3 rounded-lg border border-[#9B9B9B] placeholder:text-[#9B9B9B] "
+            type="text"
+            value={subjectText}
+            onChange={handleSubjectTextChange}
+            placeholder="멤버들과 그릴 그림 주제를 입력해주세요!"
+          />
+        </form>
         <div className="flex gap-[10px] text-sm mt-[14px]">
           {hashtagArr.map((hashtag, index) => (
             <div key={`${hashtag}-${index}`}>
@@ -74,7 +95,8 @@ export default function CreateRoom() {
           ))}
         </div>
         <div className="flex justify-center items-center w-[93px] h-[31px] rounded-[4px] bg-[#F2F2F2] text-sm mt-[20px]">
-          <span className="text-[#9B9B9B]">예시문장보기</span>
+          <button className="text-[#9B9B9B]">예시문장보기</button>
+          {/* 예시문장 들어갈 곳 */}
         </div>
       </div>
       <div className="h-[94px] mt-[64px] " onWheel={moseoverHandler}>
@@ -82,34 +104,29 @@ export default function CreateRoom() {
           <span>참여 멤버 수</span>
           <span>최대 6명</span>
         </div>
-        <div
-          className="mt-[16px] flex gap-[10px] overflow-x-auto overflow-y-hidden whitespace-nowrap scroll-smooth scrollbar-hide"
+        <form
+          className="mt-[16px] w-full flex h-fit gap-[10px] overflow-x-auto overflow-y-hidden whitespace-nowrap scroll-smooth scrollbar-hide"
           id="slider"
-          ref={sliderRef}
         >
-          <form
-            className="mt-[16px] w-full flex h-fit gap-[10px] overflow-x-auto overflow-y-hidden whitespace-nowrap scroll-smooth scrollbar-hide"
-            id="drawingTimeSlider"
-          >
-            {memberNumArr.map((num, i) => (
-              <div className="w-[40rem]" key={`${i}`}>
-                <input
-                  type="radio"
-                  className="peer hidden"
-                  id={`${num}-${i}`}
-                  value={num}
-                  name="answer"
-                />
-                <label
-                  htmlFor={`${num}-${i}`}
-                  className="block cursor-pointer select-none rounded-lg px-[25px] py-[10px] border border-[#DEDEDE] text-center peer-checked:bg-black peer-checked:text-white"
-                >
-                  {i + 1}명
-                </label>
-              </div>
-            ))}
-          </form>
-        </div>
+          {memberNumArr.map((num, i) => (
+            <div className="w-[40rem]" key={`${i}`} id="item">
+              <input
+                type="radio"
+                className="peer hidden"
+                id={`${num}-${i}`}
+                value={num}
+                name="answer"
+                onClick={handleMemberCheck}
+              />
+              <label
+                htmlFor={`${num}-${i}`}
+                className="block cursor-pointer select-none rounded-lg px-[29.53px] py-[11px] border border-[#DEDEDE] text-center peer-checked:bg-black peer-checked:text-white"
+              >
+                {i + 1}명
+              </label>
+            </div>
+          ))}
+        </form>
       </div>
       <div
         className="h-[125px] mt-[36px] mb-[59px] relative"
@@ -119,32 +136,35 @@ export default function CreateRoom() {
           <span>드로잉 시간</span>
         </div>
         <div>
-          <div
+          <form
             className="mt-[16px] flex gap-[10px] overflow-x-auto overflow-y-hidden whitespace-nowrap scroll-smooth scrollbar-hide"
             id="drawingTimeSlider"
-            ref={timeSetSliderRef}
           >
             {drawingTime.map((time, i) => (
-              <div className="w-[40rem]" key={`${time}-${i}`}>
+              <div className="w-[40rem]" key={`${time}-${i}`} id="timeItem">
                 <input
                   type="radio"
                   className="peer hidden"
                   id={`${time}-${i}`}
                   value={time}
                   name="answer"
+                  onClick={handleTimeCheck}
                 />
                 <label
                   htmlFor={`${time}-${i}`}
-                  className="block cursor-pointer select-none rounded-lg p-3 border border-[#DEDEDE] text-center peer-checked:bg-black peer-checked:text-white"
+                  className="block cursor-pointer select-none rounded-lg px-[30.5px] py-[11px] border border-[#DEDEDE] text-center peer-checked:bg-black peer-checked:text-white"
                 >
                   {time}
                 </label>
               </div>
             ))}
-          </div>
+          </form>
         </div>
       </div>
-      <LargeBtn text="시작하기" active={false} />
+      <LargeBtn
+        text="시작하기"
+        active={subjectText.length && memberOption && timeOption ? true : false}
+      />
     </div>
   );
 }
--- src/components/LargeBtn.tsx
+++ src/components/LargeBtn.tsx
@@ -5,7 +5,7 @@ interface BtnText {
 export default function LargeBtn({ text, active }: BtnText) {
   return (
     <button
-      className={`w-[332px] h-[64px] ${active ? 'bg-[#E0FF68]' : 'bg-[#D9D9D9]'} text-black text-[20px] rounded-md text-center flex justify-center items-center `}
+      className={`w-[332px] h-[64px] ${active ? 'bg-[#E0FF68]' : 'bg-[#D9D9D9]'} text-black text-[20px] rounded-md text-center flex justify-center items-center ${active ? 'cursor-pointer' : 'cursor-default'}`}
     >
       {text}
     </button>
