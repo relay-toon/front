60373e523c72e12e81c48c353f9c24b02e0e49cd
unknown <dhdytjq2@gmail.com>
2024-04-21 17:26:49 +0900

    design: mypage-sidebar

---
+++ src/app/(main)/comptest/page.tsx
@@ -0,0 +1,9 @@
+import MyPageSideBar from "@/src/components/Mypage-sidebar";
+
+export default function CompTest (){
+    return(
+        <div>
+            <MyPageSideBar />
+        </div>
+    )
+}
