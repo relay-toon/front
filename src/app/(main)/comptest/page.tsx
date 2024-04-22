01d5b7639cd1543de2512c20c9f27f39a1f06730
unknown <dhdytjq2@gmail.com>
2024-04-22 11:18:07 +0900

    chore: branch component resetting

--- src/app/(main)/comptest/page.tsx
+++ src/app/(main)/comptest/page.tsx
@@ -1,11 +1,11 @@
-import MyPageNonLoginSideBar from "@/src/components/Mypage-nonLoginSideBar";
+
 
 
 
 export default function CompTest() {
   return (
     <div>
-      <MyPageNonLoginSideBar />
+
     </div>
   );
 }
