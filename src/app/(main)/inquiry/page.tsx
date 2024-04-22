bf21a320ac0b6eea4118f13878d2d2e8587022f7
unknown <dhdytjq2@gmail.com>
2024-04-22 13:36:06 +0900

    design: inquiry page

---
+++ src/app/(main)/inquiry/page.tsx
@@ -0,0 +1,52 @@
+'use client';
+import LargeBtn from '@/src/components/LargeBtn';
+import BackHeader from '@/src/components/header/BackHeader';
+import { ChangeEvent, useState } from 'react';
+
+export default function Inquiry() {
+  const [isActive, setIsActive] = useState(false);
+  const [inquiry, setInquiry] = useState('');
+  const onInquiryTextChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setInquiry(e.target.value);
+  };
+
+  return (
+    <div className="*:custom-pretendard-font relative flex h-screen flex-col items-center bg-[#F7F7F7] px-[20px]">
+      <div className="flex h-[48px] items-center justify-center bg-[#F7F7F7]">
+        <div className="absolute left-0">
+          <BackHeader />
+        </div>
+        <span className="custom-pretendard-font text-[18px] font-bold">
+          건의하기
+        </span>
+      </div>
+
+      <div className="mt-[35px] flex h-[90px] flex-col gap-[16px]">
+        <div>
+          <span className="text-[16px] font-bold">문의유형</span>
+        </div>
+        <div className="h-[50px] w-[350px] rounded-[8px] bg-white p-4 text-[16px] font-semibold">
+          <span>건의하기</span>
+        </div>
+      </div>
+
+      <div className="mt-[28px] flex flex-col gap-[16px]">
+        <div>
+          <span className="text-[16px] font-bold">문의내용</span>
+        </div>
+        <textarea
+          value={inquiry}
+          onChange={onInquiryTextChange}
+          placeholder="릴레이툰 팀에게 건의할 사항을 입력해주세요"
+          className="h-[230px] w-[349px] rounded-[12px] p-4 placeholder:text-[15px] placeholder:font-medium"
+        />
+      </div>
+      <div className="mt-[150px]">
+        <LargeBtn
+          text="등록하기"
+          active={inquiry.length ? !isActive : isActive}
+        />
+      </div>
+    </div>
+  );
+}
--- src/components/LargeBtn.tsx
+++ src/components/LargeBtn.tsx
@@ -5,7 +5,7 @@ interface BtnText {
 export default function LargeBtn({ text, active }: BtnText) {
   return (
     <button
-      className={`w-[332px] h-[64px] ${active ? 'bg-[#E0FF68]' : 'bg-[#D9D9D9]'} text-black text-[20px] rounded-md text-center flex justify-center items-center ${active ? 'cursor-pointer' : 'cursor-default'}`}
+      className={`custom-waguri-font h-[64px] w-[332px] ${active ? 'bg-[#E0FF68]' : 'bg-[#D9D9D9]'} flex items-center justify-center rounded-md text-center text-[20px] text-black ${active ? 'cursor-pointer' : 'cursor-default'}`}
     >
       {text}
     </button>
