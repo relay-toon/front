dd21b13d4f7c153ac5dff67e64cbbff516db2033
Namhyun <jenple9709@gmail.com>
2024-04-23 21:50:53 +0900

    design: font size,bold and text color change in Item page

--- src/app/(main)/item/[id]/page.tsx
+++ src/app/(main)/item/[id]/page.tsx
@@ -15,8 +15,15 @@ export default function ItemPage() {
       <div className="align-center ml-auto mr-auto flex w-[280px] rounded-md bg-white text-center">
         <div className="flex-start flex px-[18.5px] py-3 text-xs">테스트</div>
       </div>
-      <div className="ml-auto mr-auto mt-4 flex h-[364px] w-[280px] items-center rounded-md bg-white"></div>
-      <div className="my-[15px] ml-auto mr-auto flex h-14 w-[350px] flex-row content-between items-center justify-between bg-[#EAEAEA] px-[15px]">
+      <div className="ml-auto mr-auto mt-4 flex h-[364px] w-[280px] items-center rounded-md bg-white">
+        <Image
+          src="/img/draw-example.png"
+          alt="picture-gallery"
+          width={280}
+          height={280}
+        />
+      </div>
+      <div className="my-[15px] ml-auto mr-auto flex h-14 w-[350px] flex-row content-between items-center justify-between bg-[#EAEAEA] px-[15px] text-base font-semibold text-[#666666]">
         <div>참여 멤버</div>
         <div className=" flex flex-row items-center">
           4명
