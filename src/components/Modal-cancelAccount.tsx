9ac5aa740c2ec48981fbbe0c1770db8d1fc4ce20
unknown <dhdytjq2@gmail.com>
2024-04-23 13:44:55 +0900

    complete all modals

--- src/components/Modal-cancelAccount.tsx
+++ src/components/Modal-cancelAccount.tsx
@@ -1,4 +1,7 @@
 export default function ModalCancelAccount() {
+  const onQuitClick = () => {};
+  const onAccountCancelClick = () => {};
+
   return (
     <div className="*:custom-pretendard-font flex h-[189px] w-[300px] flex-col items-center rounded-[12px] bg-white">
       <div className="flex h-[129px] w-[300px] flex-col items-center justify-center gap-[8px] font-medium">
@@ -8,10 +11,16 @@ export default function ModalCancelAccount() {
         </span>
       </div>
       <div className="border-t-F2F2F2 flex h-[60px] border-t border-t-[#F2F2F2] text-[16px] font-bold ">
-        <div className="flex w-[150px] cursor-pointer items-center justify-center">
+        <div
+          onClick={onQuitClick}
+          className="flex w-[150px] cursor-pointer items-center justify-center"
+        >
           <span>취소</span>
         </div>
-        <div className="flex w-[150px] cursor-pointer items-center justify-center text-[#D84D45]">
+        <div
+          onClick={onAccountCancelClick}
+          className="flex w-[150px] cursor-pointer items-center justify-center text-[#D84D45]"
+        >
           <span>탈퇴하기</span>
         </div>
       </div>
--- src/components/modal-logIn.tsx
+++ src/components/modal-logIn.tsx
@@ -1,14 +1,17 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-
-export default function LoginModal() {
+export default function ModalLogin() {
+  const onLoginClick = () => {};
   return (
-    <div className="w-[273px] h-[220.27px]  bg-white flex flex-col justify-center items-center rounded-[12px] gap-[32px] custom-waguri-font">
-      <span className="text-center text-xl">
+    <div className="custom-pretendard-font flex  h-[220.27px] w-[273px] flex-col items-center justify-center gap-[32px] rounded-[12px] bg-white">
+      <span className="text-center text-xl font-medium">
         릴레이툰 시작 전 <br /> 로그인을 해주세요!
       </span>
       <Link href={'/login'}>
-        <button className="w-[140px] h-[50px] text-lg bg-black text-white rounded-[6px]">
+        <button
+          className="h-[50px] w-[140px] rounded-[6px] bg-black text-lg font-bold text-white"
+          onClick={onLoginClick}
+        >
           로그인 하기
         </button>
       </Link>
