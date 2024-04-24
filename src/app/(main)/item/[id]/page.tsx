a656dffc40800b9c88f12baa582203aa478568f3
Namhyun <jenple9709@gmail.com>
2024-04-24 16:44:11 +0900

    feat: Add MemberModal component to display participant detailsAdd eraser in draw functionality

--- src/app/(main)/item/[id]/page.tsx
+++ src/app/(main)/item/[id]/page.tsx
@@ -1,15 +1,45 @@
+'use client';
 import SaveButton from '@/src/components/SaveButton';
 import ShareButton from '@/src/components/ShareButton';
 import BackHeader from '@/src/components/header/BackHeader';
 import Image from 'next/image';
+import { useState } from 'react';
+import MemberModal from '../_component/MemberModal';
 
 export default function ItemPage() {
   const now = Date.now();
   const date = new Date(now).toLocaleDateString();
+  const [showModal, setShowModal] = useState(false);
+  const handleCloseModal = (e: React.MouseEvent) => {
+    if (e.target === e.currentTarget) {
+      setShowModal(false);
+    }
+  };
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+  const userName = [
+    '가나다라마바사아자차카타',
+    '총닉네임 12자',
+    '유저C',
+    '유저D',
+    '유저E',
+    '유저F',
+    '유저G',
+    '유저H',
+  ];
   return (
-    <div className="mb-[112px]">
+    <div className="relative mb-[100px]">
+      {showModal && (
+        <div
+          className="fixed inset-0 z-30 flex items-center justify-center bg-black bg-opacity-50"
+          onClick={handleCloseModal}
+        >
+          <MemberModal users={userName} onClick={(e) => e.stopPropagation()} />
+        </div>
+      )}
       <BackHeader text="그림상세" />
-      <div className="text-base] align-center mt-4 flex justify-center text-[#9E9E9E]">
+      <div className="align-center mt-4 flex justify-center text-base text-[#9E9E9E]">
         {date}
       </div>
       <div className="align-center ml-auto mr-auto flex w-[280px] rounded-md bg-white text-center">
@@ -23,10 +53,13 @@ export default function ItemPage() {
           height={280}
         />
       </div>
-      <div className="my-[15px] ml-auto mr-auto flex h-14 w-[350px] flex-row content-between items-center justify-between bg-[#EAEAEA] px-[15px] text-base font-semibold text-[#666666]">
+      <div className="my-[15px] ml-auto mr-auto flex h-14 w-[350px] flex-row content-between items-center justify-between rounded-lg bg-[#EAEAEA] px-[23px] text-base font-semibold text-[#666666]">
         <div>참여 멤버</div>
-        <div className=" flex flex-row items-center">
-          4명
+        <div
+          className=" flex cursor-pointer flex-row items-center"
+          onClick={handleOpenModal}
+        >
+          {userName.length}명
           <Image
             src="/svg/right-arrow.svg"
             alt="arrow"
@@ -35,7 +68,7 @@ export default function ItemPage() {
           />
         </div>
       </div>
-      <div className="mt-5 flex justify-center gap-[14px]">
+      <div className="relative mt-5 flex justify-center gap-[14px]">
         <SaveButton />
         <ShareButton />
       </div>
