f1da96c7b0c476758941e140e7529107764e87f9
unknown <dhdytjq2@gmail.com>
2024-04-24 22:34:15 +0900

    pull origin master

--- src/app/(main)/comptest/page.tsx
+++ src/app/(main)/comptest/page.tsx
@@ -1,12 +1,8 @@
-import ModalLogout from '@/src/components/Modal-Logout';
-import ModalNickname from '@/src/components/Modal-nickname';
-import ModalLogin from '@/src/components/modal-logIn';
-import ModalPainterName from '@/src/components/modal-painterName';
+
 
 export default function CompTest() {
   return (
     <div>
-      <ModalPainterName />
     </div>
   );
 }
