2fd16d0a6cee04950231a9d0888440711e504db6
Namhyun <jenple9709@gmail.com>
2024-05-06 19:25:22 +0900

    feat: Pagenation in useGetMyCreatedToon and useGetMyParticpated hooks

--- src/hooks/useGetMyParticipatedToon.ts
+++ src/hooks/useGetMyParticipatedToon.ts
@@ -2,16 +2,25 @@ import { AxiosInstance } from 'axios';
 import { useAxios } from '../lib/axios';
 import { useQuery } from '@tanstack/react-query';
 
-const getMyParticipatedToon = async (axiosInstance: AxiosInstance) => {
-  const response = await axiosInstance.get('/toons/participated');
+const getMyParticipatedToon = async (
+  axiosInstance: AxiosInstance,
+  page: number,
+  completed: boolean,
+) => {
+  const response = await axiosInstance.get('/toons/participated', {
+    params: {
+      page,
+      completed,
+    },
+  });
   return response.data;
 };
 
-export const useGetMyParticipatedToon = () => {
+export const useGetMyParticipatedToon = (page: number, completed: boolean) => {
   const { axiosInstance } = useAxios();
   return useQuery({
-    queryKey: ['myParticipatedToon'],
-    queryFn: () => getMyParticipatedToon(axiosInstance),
+    queryKey: ['myParticipatedToon', page],
+    queryFn: () => getMyParticipatedToon(axiosInstance, page, completed),
     staleTime: 1000 * 60 * 30,
   });
 };
