a4db3d508ee5c036fab169f3d7f40e7b933dee50
Namhyun <jenple9709@gmail.com>
2024-05-10 23:34:52 +0900

    feat: Add data in PrevPicture.tsx, DrawingOrder.tsx

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -2,12 +2,14 @@ import Image from 'next/image';
 
 export default function LoadingSpinner() {
   return (
-    <Image
-      src="/svg/loading.svg"
-      alt="loading"
-      width={82}
-      height={79}
-      className=" ml-auto mr-auto flex animate-spin-2s items-center justify-center"
-    />
+    <div className="flex h-[754px] w-full">
+      <Image
+        src="/svg/loading.svg"
+        alt="loading"
+        width={82}
+        height={79}
+        className="ml-auto mr-auto flex h-full animate-spin-2s items-center"
+      />
+    </div>
   );
 }
