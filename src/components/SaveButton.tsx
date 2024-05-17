193366d377fd781f7e9b51e17c10451291e67fba
Namhyun <jenple9709@gmail.com>
2024-05-17 12:40:55 +0900

    feat: Implement download functionality

--- src/components/SaveButton.tsx
+++ src/components/SaveButton.tsx
@@ -1,7 +1,17 @@
-import '@/src/app/styles/globals.css';
+'use client';
+
+import { useParams } from 'next/navigation';
 export default function SaveButton() {
+  const { id } = useParams();
+  const awsLink = `https://relaytoon-dev.s3.ap-northeast-2.amazonaws.com/toons/${id}.png`;
+  const onClick = () => {
+    window.location.href = awsLink;
+  };
   return (
-    <button className=" custom-waguri-font h-[58px] w-[168px] rounded-[10px] bg-black text-white hover:bg-[#9B9B9B]">
+    <button
+      className=" custom-waguri-font h-[58px] w-[168px] rounded-[10px] bg-black text-white hover:bg-[#9B9B9B]"
+      onClick={onClick}
+    >
       저장하기
     </button>
   );
