c96fe6a43b2dbfdad05243cc5a8902a825b9d32d
Namhyun <jenple9709@gmail.com>
2024-05-23 19:09:25 +0900

    feat: Change url to query parameter and pagination ui

--- src/app/(main)/my-gallery/page.tsx
+++ src/app/(main)/my-gallery/page.tsx
@@ -4,26 +4,31 @@ import { useGetMyCreatedToon } from '@/src/hooks/useGetMyCreatedToon';
 import { useGetMyParticipatedToon } from '@/src/hooks/useGetMyParticipatedToon';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
+import { useRouter, useSearchParams } from 'next/navigation';
+
 export default function MyGallery() {
-  const [tab, setTab] = useState<'만든 그림' | '참여 그림'>('만든 그림');
-  const [pageNumber, setPageNumber] = useState(1);
+  const searchParams = useSearchParams();
+  const initialPage = parseInt(searchParams.get('page') || '1', 10);
+  const initialTab =
+    (searchParams.get('tab') as 'create' | 'participate') || 'create';
+  const [tab, setTab] = useState<'create' | 'participate'>(initialTab);
+  const [pageNumber, setPageNumber] = useState(initialPage);
   const [totalPages, setTotalPages] = useState(1);
+
   const [completed, setCompleted] = useState(false);
   const maxButtons = 5;
-  const halfMaxButtons = Math.floor(maxButtons / 2);
 
-  let startPage, endPage;
+  const router = useRouter();
 
-  if (pageNumber <= halfMaxButtons) {
-    startPage = 1;
-    endPage = Math.min(totalPages, maxButtons);
-  } else if (pageNumber + halfMaxButtons >= totalPages) {
-    startPage = Math.max(1, totalPages - maxButtons + 1);
-    endPage = totalPages;
-  } else {
-    startPage = pageNumber - halfMaxButtons;
-    endPage = pageNumber + halfMaxButtons;
-  }
+  const calculatePageRange = () => {
+    const currentSet = Math.ceil(pageNumber / maxButtons);
+    const startPage = (currentSet - 1) * maxButtons + 1;
+    const endPage = Math.min(currentSet * maxButtons, totalPages);
+    return { startPage, endPage };
+  };
+
+  const { startPage, endPage } = calculatePageRange();
+
   const { data: myCreatedToon, refetch: refetchCreated } = useGetMyCreatedToon(
     pageNumber,
     completed,
@@ -31,27 +36,41 @@ export default function MyGallery() {
 
   const { data: myParticipatedToon, refetch: refetchParticipated } =
     useGetMyParticipatedToon(pageNumber, completed);
-  const handleTabChange = (newTab: '만든 그림' | '참여 그림') => {
+
+  const handleTabChange = (newTab: 'create' | 'participate') => {
     if (newTab !== tab) {
       setTab(newTab);
       setPageNumber(1);
-      setCompleted(newTab === '참여 그림');
+      router.push(`?tab=${newTab}&page=1`);
     }
   };
-  console.log(myCreatedToon, myParticipatedToon);
+
   const handlePageChange = (newPage: number) => {
-    if (newPage !== pageNumber) {
+    if (newPage !== pageNumber && newPage >= 1 && newPage <= totalPages) {
       setPageNumber(newPage);
+      router.push(`?tab=${tab}&page=${newPage}`);
     }
-    window.scrollTo({
-      top: window.scrollY,
-      behavior: 'auto',
-    });
   };
+
+  const handleNextPageSet = () => {
+    handlePageChange(pageNumber + 1);
+  };
+
+  const handlePrevPageSet = () => {
+    handlePageChange(pageNumber - 1);
+  };
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const page = parseInt(params.get('page') || '1', 10);
+    const tab = (params.get('tab') as 'create' | 'participate') || 'create';
+    setTab(tab);
+    setPageNumber(page);
+  }, [searchParams]);
+
   useEffect(() => {
     if (pageNumber === 1) {
       const newTotalPages =
-        (tab === '만든 그림'
+        (tab === 'create'
           ? myCreatedToon?.totalPage
           : myParticipatedToon?.totalPage) || 1;
       setTotalPages(newTotalPages);
@@ -59,7 +78,7 @@ export default function MyGallery() {
   }, [tab, myCreatedToon, myParticipatedToon]);
 
   useEffect(() => {
-    if (tab === '만든 그림') {
+    if (tab === 'create') {
       refetchCreated();
     } else {
       refetchParticipated();
@@ -68,6 +87,7 @@ export default function MyGallery() {
 
   const createdToons = myCreatedToon?.toons || [];
   const participatedToons = myParticipatedToon?.toons || [];
+
   return (
     <div className="min-h-[754px]">
       <div className="flex">
@@ -75,13 +95,13 @@ export default function MyGallery() {
       </div>
       <div className="relative mt-[64px] flex items-center justify-center text-base font-bold">
         <div
-          onClick={() => handleTabChange('만든 그림')}
+          onClick={() => handleTabChange('create')}
           className={`relative mx-[60px] mb-1 cursor-pointer ${
-            tab === '만든 그림' ? 'text-[#595959]' : 'text-[#9E9E9E]'
+            tab === 'create' ? 'text-[#595959]' : 'text-[#9E9E9E]'
           }`}
         >
           만든 그림
-          {tab === '만든 그림' && (
+          {tab === 'create' && (
             <span
               className="absolute -bottom-1 left-1/2 block h-[3px] bg-black"
               style={{ width: 78, transform: 'translateX(-50%)' }}
@@ -89,13 +109,13 @@ export default function MyGallery() {
           )}
         </div>
         <div
-          onClick={() => handleTabChange('참여 그림')}
+          onClick={() => handleTabChange('participate')}
           className={`relative mx-[60px] mb-1 cursor-pointer ${
-            tab === '참여 그림' ? 'text-[#595959]' : 'text-[#9E9E9E]'
+            tab === 'participate' ? 'text-[#595959]' : 'text-[#9E9E9E]'
           }`}
         >
           참여 그림
-          {tab === '참여 그림' && (
+          {tab === 'participate' && (
             <span
               className="absolute -bottom-1 left-1/2 block h-[3px] bg-black"
               style={{ width: 78, transform: 'translateX(-50%)' }}
@@ -104,7 +124,7 @@ export default function MyGallery() {
         </div>
       </div>
       <hr className="border-1 border-solid border-[#C7C7C7]" />
-      {tab === '만든 그림' ? (
+      {tab === 'create' ? (
         <>
           <div className="ml-5 mt-[29px] font-bold">
             <span>총 {createdToons.length}장</span>
@@ -181,6 +201,20 @@ export default function MyGallery() {
       )}
 
       <div className="mt-5 flex justify-center">
+        {pageNumber !== 1 ? (
+          <button onClick={handlePrevPageSet} className="mx-1 p-2 text-lg">
+            {'<'}
+          </button>
+        ) : (
+          <button
+            onClick={handlePrevPageSet}
+            className="mx-1 p-2 text-lg text-neutral-400"
+            disabled
+          >
+            {'<'}
+          </button>
+        )}
+
         {Array.from({ length: endPage - startPage + 1 }, (_, i) => (
           <button
             key={startPage + i}
@@ -190,6 +224,20 @@ export default function MyGallery() {
             {startPage + i}
           </button>
         ))}
+
+        {pageNumber !== totalPages ? (
+          <button onClick={handleNextPageSet} className="mx-1 p-2 text-lg">
+            {'>'}
+          </button>
+        ) : (
+          <button
+            onClick={handleNextPageSet}
+            className="mx-1 p-2 text-lg text-neutral-400"
+            disabled
+          >
+            {'>'}
+          </button>
+        )}
       </div>
     </div>
   );
