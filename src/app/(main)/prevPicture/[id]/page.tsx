cd1d7a873dddccb1a07aa7c702d09b5f4218169c
Namhyun <jenple9709@gmail.com>
2024-05-30 20:25:37 +0900

    chore: Metadata set

--- src/app/(main)/prevPicture/[id]/page.tsx
+++ src/app/(main)/prevPicture/[id]/page.tsx
@@ -2,7 +2,7 @@ import PrevPicture from '../_component/PrevPicture';
 
 export const metadata = {
   title: '로비 / 릴레이툰',
-  description: '로비입니다.',
+  description: '로비 페이지입니다.',
 };
 export default function PrevPicturePage() {
   return <PrevPicture />;
