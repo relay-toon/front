0791361d063c7225b8239f50346057754cf24498
Namhyun <jenple9709@gmail.com>
2024-05-31 22:06:27 +0900

    CSS,JS 난독화

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+import CssMinimizerPlugin from 'css-minimizer-webpack-plugin';
+import TerserPlugin from 'terser-webpack-plugin';
+
 const nextConfig = {
   images: {
     domains: ['relaytoon-dev.s3.ap-northeast-2.amazonaws.com'],
@@ -19,25 +22,20 @@ const nextConfig = {
 
     if (!dev) {
       config.optimization.minimize = true;
+      config.optimization.minimizer = [
+        new TerserPlugin({
+          terserOptions: {
+            compress: {
+              drop_console: true,
+            },
+          },
+        }),
+        new CssMinimizerPlugin(),
+      ];
       config.optimization.splitChunks = {
         chunks: 'all',
         minSize: 0,
       };
-
-      config.module.rules.forEach((rule) => {
-        if (rule.test && rule.test.toString().includes('css')) {
-          rule.use.forEach((use) => {
-            if (use.loader && use.loader.includes('css-loader')) {
-              use.options = {
-                ...use.options,
-                modules: {
-                  localIdentName: '[hash:base64:8]',
-                },
-              };
-            }
-          });
-        }
-      });
     }
 
     return config;
