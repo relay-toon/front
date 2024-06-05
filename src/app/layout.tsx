b708fe559dd176bf22969e430ca75746178a8a97
Namhyun <jenple9709@gmail.com>
2024-06-05 18:43:09 +0900

    툰 상세페이지 메타데이터 에러 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './styles/globals.css';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
   title: '릴레이툰',
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
